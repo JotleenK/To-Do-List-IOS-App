944bf4c0182257a2d9ebe71045ef9a481e989a8d
Jotleen <jotjot2109@gmail.com>
2024-01-09 12:56:02 +1300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Jotleen Kaur on 9/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
